a0f1a2b0df236a6f3e2812ce4d9c95660f91d4b2
zang.loo <zhanglu@lzrj.net>
2023-10-17 20:30:06 +0800

    replace xmltree by roxmltree

--- src/book/epub.rs
+++ src/book/epub.rs
@@ -6,11 +6,10 @@ use std::io::Cursor;
 use std::io::Read;
 use std::io::Seek;
 use std::path::PathBuf;
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, bail, Result};
 use elsa::FrozenMap;
 use indexmap::IndexSet;
-use strip_bom::StripBom;
-use xmltree::{Element, XMLNode};
+use roxmltree::{Children, Document, ExpandedName, Node};
 use zip::ZipArchive;
 
 use crate::book::{Book, LoadingChapter, ChapterError, Line, Loader, TocInfo};
@@ -266,10 +265,11 @@ impl<R: Read + Seek + 'static> EpubBook<R> {
 			return Err(anyhow!("Encrypted epub."));
 		}
 		let container_text = zip_string(&mut zip, "META-INF/container.xml")?;
-		let container = Element::parse(container_text.strip_bom().as_bytes())?;
-		let rootfiles = container.get_child("rootfiles").ok_or(anyhow!("invalid container.xml: no rootfiles"))?;
-		let rootfile = rootfiles.get_child("rootfile").ok_or(anyhow!("invalid container.xml: no rootfile"))?;
-		let content_opf_path = rootfile.attributes.get("full-path").ok_or(anyhow!("invalid container.xml: no full-path"))?;
+		let doc = Document::parse(&container_text)?;
+		let root = doc.root_element();
+		let rootfiles = get_child(root, "rootfiles").ok_or(anyhow!("invalid container.xml: no rootfiles"))?;
+		let rootfile = get_child(rootfiles, "rootfile").ok_or(anyhow!("invalid container.xml: no rootfile"))?;
+		let content_opf_path = rootfile.attribute("full-path").ok_or(anyhow!("invalid container.xml: no full-path"))?;
 		let content_opf_dir = match PathBuf::from(&content_opf_path).parent() {
 			Some(p) => p.to_path_buf(),
 			None => PathBuf::new(),
@@ -426,24 +426,22 @@ fn zip_content<R: Read + Seek>(zip: &mut ZipArchive<R>, name: &str) -> Result<Ve
 	}
 }
 
-fn parse_nav_points(nav_points_element: &Element, level: usize, nav_points: &mut Vec<NavPoint>, cwd: &PathBuf)
+fn parse_nav_points(nav_points_element: Node, level: usize, nav_points: &mut Vec<NavPoint>, cwd: &PathBuf)
 {
-	fn parse_element(el: &Element, level: usize, cwd: &PathBuf) -> Option<NavPoint> {
-		let id = Some(el.attributes.get("id")?.to_string());
+	fn parse_element(el: Node, level: usize, cwd: &PathBuf) -> Option<NavPoint> {
+		let id = Some(el.attribute("id")?.to_string());
 		let play_order: Option<usize> = el
-			.attributes
-			.get("playOrder")
+			.attribute("playOrder")
 			.and_then(|po| po.parse().ok());
-		let src = el.get_child("content")?.attributes.get("src")?.to_string();
+		let src = get_child(el, "content")?.attribute("src")?.to_string();
 		let mut src_split = src.split('#');
 		let src_file = src_split.next()?;
 		let src_file = concat_path(cwd.clone(), src_file)?;
 		let src_file = Some(src_file);
 		let src_anchor = src_split.next().and_then(|str| Some(String::from(str)));
-		let label = el
-			.get_child("navLabel")
-			.and_then(|el| el.get_child("text"))
-			.and_then(|el| el.get_text())
+		let label = get_child(el, "navLabel")
+			.and_then(|el| get_child(el, "text"))
+			.and_then(|el| el.text())
 			.map(|s| s.to_string());
 		Some(NavPoint {
 			id,
@@ -456,13 +454,10 @@ fn parse_nav_points(nav_points_element: &Element, level: usize, nav_points: &mut
 		})
 	}
 	nav_points_element
-		.children
-		.iter()
+		.children()
 		.filter_map(|node| {
-			if let Some(el) = node.as_element() {
-				if el.name == "navPoint" {
-					return Some(el);
-				}
+			if node.has_tag_name("navPoint") {
+				return Some(node);
 			}
 			None
 		})
@@ -476,10 +471,10 @@ fn parse_nav_points(nav_points_element: &Element, level: usize, nav_points: &mut
 
 fn parse_ncx(text: &str, cwd: &PathBuf) -> Result<Vec<NavPoint>>
 {
-	let ncx = Element::parse(text.strip_bom().as_bytes())
+	let doc = Document::parse(text)
 		.map_err(|_e| anyhow!("Failed parse ncx"))?;
-	let nav_map = ncx
-		.get_child("navMap")
+	let ncx = doc.root_element();
+	let nav_map = get_child(ncx, "navMap")
 		.ok_or_else(|| anyhow!("Missing navMap"))?;
 	let mut nav_points = vec![];
 	parse_nav_points(nav_map, 1, &mut nav_points, cwd);
@@ -494,14 +489,14 @@ fn parse_ncx(text: &str, cwd: &PathBuf) -> Result<Vec<NavPoint>>
 /// according to https://www.w3.org/publishing/epub3/epub-packages.html#sec-package-nav-def
 fn parse_nav_doc(text: &str, cwd: &PathBuf) -> Result<Vec<NavPoint>>
 {
-	fn search_nav(element: &Element) -> Option<&Element>
+	fn search_nav<'a, 'i>(element: Node<'a, 'i>, type_name: ExpandedName) -> Option<Node<'a, 'i>>
 	{
-		for child in &element.children {
-			if let Some(element) = child.as_element() {
-				if element.name == "nav" && element.attributes.get("type").map_or(false, |t| t == "toc") {
-					return Some(element);
+		for child in element.children() {
+			if child.is_element() {
+				if child.has_tag_name("nav") && child.attribute(type_name).map_or(false, |t| t == "toc") {
+					return Some(child);
 				}
-				let option = search_nav(element);
+				let option = search_nav(child, type_name);
 				if option.is_some() {
 					return option;
 				}
@@ -509,73 +504,71 @@ fn parse_nav_doc(text: &str, cwd: &PathBuf) -> Result<Vec<NavPoint>>
 		}
 		None
 	}
-	fn process(children: &Vec<XMLNode>, toc: &mut Vec<NavPoint>, level: usize, cwd: &PathBuf) -> Result<()>
+	fn process(children: Children, toc: &mut Vec<NavPoint>, level: usize, cwd: &PathBuf) -> Result<()>
 	{
 		for child in children {
-			match child {
-				XMLNode::Element(Element { name, children, .. }) if name == "li" => {
-					// li
-					//     In this order:
-					//         (span or a) [exactly 1]
-					//         ol [conditionally required]
-					let a = children.get(0).ok_or(anyhow!("Invalid entry in Navigation document"))?;
-					let a = a.as_element().ok_or(anyhow!("Invalid entry node in Navigation document"))?;
-					let label = match a.attributes.get("title") {
-						Some(title) => title.clone(),
-						None => a.get_text()
-							.ok_or(anyhow!("Navigation document entry with no text"))?
-							.to_string()
-					};
-					if label.len() == 0 {
-						return Err(anyhow!("Navigation document entry with empty text"));
-					}
-					let (src_file, src_anchor) = if let Some(href) = a.attributes.get("href") {
-						// In the case of the toc nav, landmarks nav and page-list nav, it MUST resolve to an Top-level Content Document or fragment therein.
-						let mut parts = href.split('#');
-						let src_file = parts.next()
-							.map(|a| a.to_string())
-							.ok_or(anyhow!("Navigation document entry href not resolve to an Top-level Content Document or fragment therein"))?;
-						let src_file = concat_path(cwd.clone(), &src_file);
-						let src_anchor = parts.next().map(|a| a.to_string());
-						(src_file, src_anchor)
-					} else {
-						(None, None)
-					};
+			if child.has_tag_name("li") {
+				let mut children = child.children();
+				// li
+				//     In this order:
+				//         (span or a) [exactly 1]
+				//         ol [conditionally required]
+				let a = children.next().ok_or(anyhow!("Invalid entry in Navigation document"))?;
+				if !a.is_element() {
+					bail!("Invalid entry node in Navigation document");
+				}
+				let label = match a.attribute("title") {
+					Some(title) => title,
+					None => a.text()
+						.ok_or(anyhow!("Navigation document entry with no text"))?
+				};
+				if label.len() == 0 {
+					bail!("Navigation document entry with empty text");
+				}
+				let (src_file, src_anchor) = if let Some(href) = a.attribute("href") {
+					// In the case of the toc nav, landmarks nav and page-list nav, it MUST resolve to an Top-level Content Document or fragment therein.
+					let mut parts = href.split('#');
+					let src_file = parts.next()
+						.map(|a| a.to_string())
+						.ok_or(anyhow!("Navigation document entry href not resolve to an Top-level Content Document or fragment therein"))?;
+					let src_file = concat_path(cwd.clone(), &src_file);
+					let src_anchor = parts.next().map(|a| a.to_string());
+					(src_file, src_anchor)
+				} else {
+					(None, None)
+				};
 
-					toc.push(NavPoint {
-						id: None,
-						label: Some(label),
-						play_order: None,
-						level,
-						src_file,
-						src_anchor,
-						first_chapter_index: 0,
-					});
-					if let Some(node) = children.get(1) {
-						match node {
-							XMLNode::Element(Element { name, children, .. }) if name == "ol" =>
-								process(children, toc, level + 1, cwd)?,
-							_ => {}
-						}
+				toc.push(NavPoint {
+					id: None,
+					label: Some(label.to_owned()),
+					play_order: None,
+					level,
+					src_file,
+					src_anchor,
+					first_chapter_index: 0,
+				});
+				if let Some(node) = children.next() {
+					if node.has_tag_name("ol") {
+						process(node.children(), toc, level + 1, cwd)?;
 					}
 				}
-				_ => {}
 			}
 		}
 		Ok(())
 	}
-	let document = Element::parse(text.strip_bom().as_bytes())
+	let doc = Document::parse(text)
 		.map_err(|_e| anyhow!("Failed parse Navigation document"))?;
-	let body = document.get_child("body").ok_or(anyhow!("Navigation document without body"))?;
-	let nav = search_nav(body).ok_or(anyhow!("Navigation document without nav of toc"))?;
+	let root = doc.root_element();
+	let body = get_child(root, "body").ok_or(anyhow!("Navigation document without body"))?;
+	let namespace = root.lookup_namespace_uri(Some("epub"))
+		.ok_or(anyhow!("Navigation document without epub namespace"))?;
+	let epub_type_name = ExpandedName::from((namespace, "type"));
+	let nav = search_nav(body, epub_type_name).ok_or(anyhow!("Navigation document without nav of toc"))?;
 	let mut toc = vec![];
-	for child in &nav.children {
-		match child {
-			XMLNode::Element(Element { name, children, .. }) if name == "ol" => {
-				process(children, &mut toc, 1, cwd)?;
-				break;
-			}
-			_ => {}
+	for child in nav.children() {
+		if child.has_tag_name("ol") {
+			process(child.children(), &mut toc, 1, cwd)?;
+			break;
 		}
 	}
 	if toc.len() == 0 {
@@ -585,27 +578,24 @@ fn parse_nav_doc(text: &str, cwd: &PathBuf) -> Result<Vec<NavPoint>>
 	}
 }
 
-fn parse_manifest(manifest: &Element, path: &PathBuf) -> Manifest
+fn parse_manifest(manifest: Node, path: &PathBuf) -> Manifest
 {
 	manifest
-		.children
-		.iter()
+		.children()
 		.filter_map(|node| {
-			if let Some(el) = node.as_element() {
-				if el.name == "item" {
-					let id = el.attributes.get("id")?.to_string();
-					let href = el.attributes.get("href")?;
-					let href = concat_path(path.clone(), href)?;
-					return Some((
-						id.clone(),
-						ManifestItem {
-							id,
-							href,
-							media_type: el.attributes.get("media-type")?.to_string(),
-							properties: el.attributes.get("properties").map(|s| s.to_string()),
-						},
-					));
-				}
+			if node.has_tag_name("item") {
+				let id = node.attribute("id")?.to_string();
+				let href = node.attribute("href")?;
+				let href = concat_path(path.clone(), href)?;
+				return Some((
+					id.clone(),
+					ManifestItem {
+						id,
+						href,
+						media_type: node.attribute("media-type")?.to_string(),
+						properties: node.attribute("properties").map(|s| s.to_string()),
+					},
+				));
 			}
 			None
 		})
@@ -613,43 +603,40 @@ fn parse_manifest(manifest: &Element, path: &PathBuf) -> Manifest
 }
 
 #[inline]
-fn parse_spine<R: Read + Seek>(spine: &Element, manifest: &Manifest, zip: &ZipArchive<R>) -> Option<(Spine, Option<String>)>
+fn parse_spine<R: Read + Seek>(spine: Node, manifest: &Manifest, zip: &ZipArchive<R>) -> Option<(Spine, Option<String>)>
 {
 	let file_names: HashSet<&str> = zip.file_names().collect();
-	let chapters = spine.children
-		.iter()
+	let chapters = spine.children()
 		.filter_map(|node| {
-			if let Some(el) = node.as_element() {
-				if el.name == "itemref" {
-					let id = el.attributes.get("idref")?.to_string();
-					let item = manifest.get(&id)?;
-					if file_names.contains(&item.href as &str) {
-						return Some(id);
-					}
+			if node.has_tag_name("itemref") {
+				let id = node.attribute("idref")?.to_string();
+				let item = manifest.get(&id)?;
+				if file_names.contains(&item.href as &str) {
+					return Some(id);
 				}
 			}
 			None
 		})
 		.collect();
-	let toc_id = spine.attributes.get("toc").map(|id| id.clone());
+	let toc_id = spine.attribute("toc").map(|id| id.to_owned());
 	Some((chapters, toc_id))
 }
 
 fn parse_content_opf<R: Read + Seek>(text: &str, content_opf_dir: &PathBuf, zip: &ZipArchive<R>) -> Option<ContentOPF>
 {
-	let package = Element::parse(text.strip_bom().as_bytes()).ok()?;
-	let metadata = package.get_child("metadata")?;
-	let manifest = package.get_child("manifest")?;
-	let spine = package.get_child("spine")?;
-	let title = metadata.get_child("title")?.get_text()?.to_string();
-	let author = metadata
-		.get_child("creator")
-		.map(|el| el.get_text())
+	let doc = Document::parse(text).ok()?;
+	let package = doc.root_element();
+	let metadata = get_child(package, "metadata")?;
+	let manifest = get_child(package, "manifest")?;
+	let spine = get_child(package, "spine")?;
+	let title = get_child(metadata, "title")?.text()?.to_string();
+	let author = get_child(metadata, "creator")
+		.map(|el| el.text())
 		.flatten()
-		.map(|s| s.to_string());
-	let language = metadata.get_child("language")
-		.map_or(String::new(), |e| e.get_text()
-			.map_or(String::new(), |s| s.to_string()));
+		.map(|s| s.to_owned());
+	let language = get_child(metadata, "language")
+		.map_or(String::new(), |e| e.text()
+			.map_or(String::new(), |s| s.to_owned()));
 	let manifest = parse_manifest(manifest, content_opf_dir);
 	let (spine, toc_id) = parse_spine(spine, &manifest, zip)?;
 	Some(ContentOPF {
@@ -715,4 +702,10 @@ fn path_cwd(path: &str) -> PathBuf
 	let mut cwd = PathBuf::from(path);
 	cwd.pop();
 	cwd
+}
+
+#[inline]
+fn get_child<'a, 'b>(node: Node<'a, 'b>, name: &str) -> Option<Node<'a, 'b>>
+{
+	node.children().find(|child| child.tag_name().name() == name)
 }
