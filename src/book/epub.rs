afbf585f2b6039ca21db43679681486d5cf97a21
zang.loo <zhanglu@lzrj.net>
2024-03-06 11:13:44 +0800

    incorrect calc toc index

--- src/book/epub.rs
+++ src/book/epub.rs
@@ -329,7 +329,6 @@ impl Book for EpubBook {
 					let np = &toc[toc_index];
 					match &np.src_file {
 						Some(src_file) if chapter_href == src_file => {
-							file_matched = Some(toc_index);
 							if let Some(anchor) = &np.src_anchor {
 								if let Some(position) = c.id_map.get(anchor) {
 									if position.line > line || (position.line == line && position.offset > offset) {
@@ -337,6 +336,7 @@ impl Book for EpubBook {
 									}
 								}
 							}
+							file_matched = Some(toc_index);
 						}
 						_ => if np.first_chapter_index <= self.chapter_index {
 							file_matched = Some(toc_index);
