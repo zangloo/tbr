b8f2c0407a5d3a884234c56b50d9301714cb9324
zang.loo <zhanglu@lzrj.net>
2024-04-22 11:28:58 +0800

    with zip-rs 1.1.1, there is no need store file names to check file exists.

--- src/book/epub.rs
+++ src/book/epub.rs
@@ -74,7 +74,6 @@ trait EpubArchive {
 }
 
 struct EpubZipArchive<R: Read + Seek> {
-	names: Vec<String>,
 	zip: RefCell<ZipArchive<R>>,
 }
 
@@ -83,12 +82,7 @@ impl<R: Read + Seek> EpubZipArchive<R> {
 	fn new(reader: R) -> Result<Self>
 	{
 		let zip = ZipArchive::new(reader)?;
-		let mut names = vec![];
-		for name in zip.file_names() {
-			names.push(name.to_owned());
-		}
-		names.sort();
-		Ok(EpubZipArchive { names, zip: RefCell::new(zip) })
+		Ok(EpubZipArchive { zip: RefCell::new(zip) })
 	}
 }
 
@@ -113,8 +107,7 @@ impl<R: Read + Seek> EpubArchive for EpubZipArchive<R> {
 
 	fn exists(&self, path: &str) -> bool
 	{
-		// TODO replaced when zip-rs support check existence directly
-		self.names.binary_search_by(|p| p.as_str().cmp(path)).is_ok()
+		self.zip.borrow().index_for_name(path).is_some()
 	}
 }
 
